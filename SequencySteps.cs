68cbc6c9157cf180a950780ceba19269bad7beef
Tapi200 <takwada2000@yahoo.com>
2018-03-18 16:27:40 +1100

    update cose

---
+++ SequencySteps.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeautifulArrays
+{
+    class SequencySteps
+    {
+        //Console.WriteLine("enter number of test cases");
+        private int testCases = Convert.ToInt32(Console.ReadLine());
+        private int seqSize;
+        private List<int> testSeq;
+        private int[] res = new int[5];
+        private int sum;
+        private int counta;
+
+        public void CheckSequency()
+        {
+            for (int t = 0; t <= testCases; t++)
+            {
+                //Console.WriteLine("enter the sequence size");
+                seqSize = Convert.ToInt32(Console.ReadLine());
+
+                //Console.WriteLine("enter the sequence");
+
+                testSeq = Console.ReadLine()
+                    .Split(' ')
+                    .Select(x => Convert.ToInt32(x))
+                    .ToList();
+
+                sum = testSeq.Sum();
+
+                counta = 0;
+
+                if (sum % 4 != 0)
+                {
+                    Console.WriteLine(-1);
+                    break;
+                }
+
+                if (res[1] > res[3])
+                {
+                    res[1] = res[1] - res[3];
+                    counta += res[3];
+                    res[3] = 0;
+                }
+                else
+                {
+                    res[3] = res[3] - res[1];
+                    counta += res[1];
+                    res[1] = 0;
+                }
+
+                counta += res[2] / 2;
+                res[2] = res[2] % 2;
+                res[2] += res[1] / 2;
+                res[2] += res[3] / 2;
+                counta += res[1] / 2;
+                counta += res[3] / 2;
+
+                Console.WriteLine(counta);
+            }
+        }
+    }
+}
